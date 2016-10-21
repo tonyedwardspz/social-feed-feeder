c31c54c4bf689d84c5cef8cc5ef49a6d8693fc5d
Tony Edwards <tony@plymouthsoftware.com>
2016-10-21 16:05:24 +0100

    CI deployment test

--- test.js
+++ test.js
@@ -3,7 +3,6 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
-      assert.equal(-1, [1,2,3,4].indexOf(5));
     });
   });
 });
