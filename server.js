ef1348d56dd831d0656daeba1ba1d6abdc56037a
Tony Edwards <tony@plymouthsoftware.com>
2016-10-25 11:21:35 +0100

    Setup routes and test api call

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var path    = require('path');
 
-var server = express();
-server.use('/public', express.static(__dirname + '/public'));
+var app = express();
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/scripts', express.static(__dirname + '/public/scripts'));
+app.use('/styles', express.static(__dirname + '/styles'));
 
-server.get('/*', function(req, res){
+app.get('/manifest.json', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/manifest.json'));
+});
+
+app.get('/user/auth', function(req, res){
+  console.log('[ROUTE] User Auth hit');
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ a: 'This is the JSON response' }));
+});
+
+// Catch all Route to the main dash
+app.get('/', function(req, res){
   console.log('[SERVER.GET] Catch all hit');
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 var port = process.env.PORT || 8080;
-server.listen(port || 8080, function() {
+app.listen(port, function() {
   console.log('server listening on port ' + port);
 });
--- src/scripts/app.es6.js
+++ src/scripts/app.es6.js
@@ -4,5 +4,23 @@
 
   let bucket = new BucketController();
 
+  fetch('/user/auth')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
 
 })();
