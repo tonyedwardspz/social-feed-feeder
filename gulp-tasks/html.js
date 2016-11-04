523ecc8d08a505dab975b5d0c1f21f6333b19396
Tony Edwards <tony@plymouthsoftware.com>
2016-11-04 18:22:39 +0000

    Fixes html gulp task
    The html task did not inline the critical css when initiated through the watch task

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var minifyHtml = require('gulp-minify-html');
+var runSequence = require('run-sequence');
 
 gulp.task('html:watch', function() {
-  gulp.watch('./src/*.html', ['html']);
+  gulp.watch('./src/*.html', function(){
+    runSequence(['html'], ['styles:inline']);
+  });
 });
+
+
 
 gulp.task('html', function() {
   return gulp.src('./src/*.html')
