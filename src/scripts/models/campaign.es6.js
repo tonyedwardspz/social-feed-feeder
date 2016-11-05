5f308e27ee0ee8d5f9def9b329d20f34ee109801
Tony Edwards <tony@plymouthsoftware.com>
2016-11-05 14:13:29 +0000

    Creates campaign from form data

--- src/scripts/models/campaign.es6.js
+++ src/scripts/models/campaign.es6.js
@@ -1,5 +1,19 @@
 class Campaign {
-  constructor() {
+  constructor(name, description, expiry, dailyPosts) {
+    this.name = name;
+    this.description = description;
+    this.expiry = expiry;
+    this.dailyPosts = dailyPosts;
+  }
 
+  static createFromForm(form) {
+    console.log(form.name.value);
+
+    return new Campaign(
+      form.name.value,
+      form.description.value,
+      form.expiry.value,
+      form.dailyPosts.value
+    );
   }
 }
