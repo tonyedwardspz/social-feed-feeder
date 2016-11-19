e396862cfcfa9779230738367a51d516b8ba136c
Tony Edwards <tony@plymouthsoftware.com>
2016-11-19 07:18:19 +0000

    Uodates sanity check

--- test.js
+++ test.js
@@ -3,10 +3,12 @@ require('require-dir')('tests/helpers');
 
 // Basic test to enable initial CI
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
+describe('Sanity', function() {
+
+  describe('#doTestsRun()', function() {
+    it('should return -1 when there is some semblance of sanity in JavaScript land', function() {
       assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
+  
 });
