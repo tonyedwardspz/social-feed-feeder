aedb77f6d9297507fa3759dd990fbe0533ca64b1
Tony Edwards <tony@plymouthsoftware.com>
2016-11-22 04:35:56 +0000

    Removes clientside debug statements in production

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -2,13 +2,18 @@ var gulp = require('gulp');
 var del = require('del');
 var runSequence = require('run-sequence');
 var babel = require('gulp-babel');
-// var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var gulpif = require('gulp-if');
+var stripDebug = require('gulp-strip-debug');
+var gutil = require('gulp-util');
 
 gulp.task('scripts:watch', function() {
   gulp.watch('./src/scripts/**/*.js', ['scripts']);
   gulp.watch(['./.eslintrc', './.eslintignore'], ['scripts']);
 });
+
+var env = gutil.env.env === 'PRODUCTION' ? true : false;
 
 gulp.task('scripts:es6', function() {
   return gulp.src(['./node_modules/babel-polyfill/dist/polyfill.min.js',
@@ -16,7 +21,8 @@ gulp.task('scripts:es6', function() {
                    './src/scripts/app.es6.js'])
     .pipe(concat('app.js'))
     .pipe(babel({ presets: ['es2015'] }))
-    // .pipe(uglify())
+    .pipe(gulpif(env, uglify()))
+    .pipe(gulpif(env, stripDebug()))
     .pipe(gulp.dest('./public/scripts'));
 });
 
