56091e27ac5c478b089af2908e286bba86fd3388
Tony Edwards <tony@plymouthsoftware.com>
2016-11-26 17:57:32 +0000

    Extract routes

--- src/scripts/app.es6.js
+++ src/scripts/app.es6.js
@@ -33,65 +33,19 @@ var app;
     postController: new PostController()
   };
 
-  app.shell.innerHTML = app.userView.loginScreen();
-  app.spinner.classList.add('hidden');
-
+  // Direct user flow when landing on page
   if (getCookie('user_auth') === 'true'){
     console.log('User authenticated');
     if (app.user === null) {
       app.user = new User(getCookie('user_id'));
+      app.dashboardController.index(app.user);
     }
-    console.log(app.user);
-
-    app.dashboardController.index(app.user);
+  } else {
+    console.log('User not authenticated');
+    app.shell.innerHTML = app.userView.loginScreen();
   }
 
+  // Routes
+  addRoutes();
 
-  app.shell.addEventListener('click', function(e){
-
-    if (e.target !== e.currentTarget) {
-      e.preventDefault();
-      if (e.target.id === 'login' ){
-        console.log('Login Button Clicked');
-        window.location = 'user/auth';
-      }
-
-
-      else if (e.target.id === 'dashboard') {
-        app.dashboardController.index(app.user);
-      }
-
-      else if (e.target.id === 'campaign_index') {
-        app.campaignController.index();
-      } else if (e.target.className === 'campaign_show'){
-        app.campaignController.show(e.target.dataset.id);
-      } else if (e.target.id === 'campaign_new') {
-        app.campaignController.new();
-      } else if (e.target.id === 'campaign_save') {
-        app.campaignController.create();
-      } else if (e.target.id === 'campaign_edit') {
-        app.campaignController.edit(e.target.dataset.campaignid);
-      } else if (e.target.id === 'campaign_delete') {
-        app.campaignController.delete(e.target.dataset.campaignid);
-      } else if (e.target.id === 'campaign_save_edit') {
-        app.campaignController.update(e.target.dataset.id);
-      }
-
-      else if (e.target.id === 'bucket_new') {
-        app.bucketController.new(e.target.dataset.campaignid);
-      } else if (e.target.id === 'bucket_save') {
-        app.bucketController.create();
-      } else if (e.target.className === 'bucket_show') {
-        app.bucketController.show(e.target.dataset.id);
-      } else if (e.target.className === 'bucket_edit') {
-        app.bucketController.edit(e.target.dataset.id);
-      } else if (e.target.className === 'bucket_delete') {
-        app.bucketController.delete(e.target.dataset.id,
-                                    e.target.dataset.name,
-                                    e.target.dataset.campaignid);
-      }
-    }
-
-
-  });
 })();
---
+++ src/scripts/config.js/routes.js
@@ -0,0 +1,69 @@
+'use strict';
+
+let addRoutes = () => {
+  app.shell.addEventListener('click', function(e){
+
+    // let any clicks bubble up to the apps shell to allow dynamic changing of
+    // content.... unless it is a post's file picker as we want the default to
+    // happen.
+    if (e.target !== e.currentTarget && e.target.id !== 'attachment') {
+      e.preventDefault();
+      if (e.target.id === 'login' ){
+        console.log('Login Button Clicked');
+        window.location = 'user/auth';
+      }
+
+
+      else if (e.target.id === 'dashboard') {
+        app.dashboardController.index(app.user);
+      }
+
+
+      else if (e.target.id === 'campaign_index') {
+        app.campaignController.index();
+      } else if (e.target.className === 'campaign_show'){
+        app.campaignController.show(e.target.dataset.id);
+      } else if (e.target.id === 'campaign_new') {
+        app.campaignController.new();
+      } else if (e.target.id === 'campaign_save') {
+        app.campaignController.create();
+      } else if (e.target.id === 'campaign_edit') {
+        app.campaignController.edit(e.target.dataset.campaignid);
+      } else if (e.target.id === 'campaign_delete') {
+        app.campaignController.delete(e.target.dataset.campaignid);
+      } else if (e.target.id === 'campaign_save_edit') {
+        app.campaignController.update(e.target.dataset.id);
+      }
+
+
+      else if (e.target.id === 'bucket_new') {
+        app.bucketController.new(e.target.dataset.campaignid);
+      } else if (e.target.id === 'bucket_save') {
+        app.bucketController.create();
+      } else if (e.target.className === 'bucket_show') {
+        app.bucketController.show(e.target.dataset.id);
+      } else if (e.target.className === 'bucket_edit') {
+        app.bucketController.edit(e.target.dataset.id);
+      } else if (e.target.className === 'bucket_delete') {
+        app.bucketController.delete(e.target.dataset.id,
+                                    e.target.dataset.name,
+                                    e.target.dataset.campaignid);
+      }
+
+      else if (e.target.id === 'post_new') {
+        app.postController.new(e.target.dataset.bucketid,
+                               e.target.dataset.campaignid);
+      } else if (e.target.id === 'post_save') {
+        app.postController.create();
+      } else if (e.target.className === 'post_show') {
+        app.postController.show(e.target.dataset.id);
+      } else if (e.target.className === 'post_edit') {
+        app.postController.edit(e.target.dataset.id);
+      } else if (e.target.className === 'post_delete') {
+        app.postController.delete(e.target.dataset.id,
+                                  e.target.dataset.name,
+                                  e.target.dataset.campaignid);
+      }
+    }
+  });
+};
