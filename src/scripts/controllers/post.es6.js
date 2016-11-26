cfb3612d3e9793a50c95a523b05abb2f442bc945
Tony Edwards <tony@plymouthsoftware.com>
2016-11-26 17:58:09 +0000

    Add basic post views

--- src/scripts/controllers/post.es6.js
+++ src/scripts/controllers/post.es6.js
@@ -6,26 +6,38 @@ class PostController extends BaseController {
   }
 
   show(id) {
-
+    console.log(`[Post Controller] Show: ${id}`);
   }
 
-  new(bucketID) {
+  new(bucketID, campaignID) {
+    console.log(`[Post Controller] New for bucket: ${bucketID}`);
+
+    let html = app.postView.new([bucketID, campaignID]);
+    this.updateShell(html);
+
+    // add event listener to file input as its been dynamically added to document
+    // var placeholder = document.getElementById('attachment_input');
+    //
+    // var fileInput = document.createElement('input');
+    // fileInput.setAttribute('type', 'file');
+    //
+    // placeholder.appendChild(fileInput);
 
   }
 
   create() {
-
+    console.log('[Post Controller] Create');
   }
 
   edit(id) {
-
+    console.log(`[Post Controller] Edit: ${id}`);
   }
 
   update() {
-
+    console.log(`[Post Controller] Update`);
   }
 
   delete(id, name, bucketID) {
-
+    console.log(`[Post Controller] Delete: ${id}`);
   }
 }
--- src/scripts/views/post.es6.js
+++ src/scripts/views/post.es6.js
@@ -5,23 +5,44 @@ class PostView {
 
   }
 
-  getIndex(posts) {
-    return `<h1>Post Index<h1>
+  index(posts) {
+    return `<h2>Post Index</h2>
             <p>This is the index of posts<p>`;
   }
 
-  getNew(post) {
-    return `<h1>New Post<h1>
-            <p>This is the new post view<p>`;
+  new(parents) {
+    return `${getBreadcrumbs('post_new', parents)}
+            <h2>New Bucket</h2>
+            <p>Add a new post to the current bucket<p>
+            <form name="form_post_new" method="POST" data-bucketid="${parents[0]}">
+
+              <label for="name">Name</label>
+              <input type="text" name="name" id="name" required/>
+
+              <label for="message">Message</label>
+              <textarea name='message' id='message'></textarea>
+
+              <label for='expiry'>Expiry Date</label>
+              <input name='expiry' id='expiry' type='date' />
+
+              <label for="attachment">Attachment</label>
+              <input type="file" name="attachment" id="attachment"/>
+
+              <input type="hidden" id="bucketID" value="${parents[0]}" />
+              <input type="hidden" id="campaignID" value="${parents[1]}" />
+
+              <button id='post_save'>Save</button>
+              <button class='bucket_show' data-id="${parents[0]}">Cancel</button>
+            </form>`;
   }
 
   show(post) {
-    return `<h1>Show Post<h1>
+    return `<h2>Show Post</h2>
             <p>This is the show post view<p>`;
   }
 
   edit(post) {
-    return `<h1>Edit Post<h1>
+    return `<h2>Edit Post</h2>
             <p>This is the edit post view<p>`;
   }
 
