89e3273f9086803191eba9572480559f57161bd8
Tony Edwards <tony@plymouthsoftware.com>
2016-12-02 21:56:33 +0000

    Adds conditional minification of html

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -3,6 +3,10 @@
 let gulp = require('gulp');
 let minifyHtml = require('gulp-minify-html');
 let runSequence = require('run-sequence');
+let gulpif = require('gulp-if');
+let gutil = require('gulp-util');
+
+let env = gutil.env.env === 'PRODUCTION' ? true : false;
 
 gulp.task('html:watch', function() {
   gulp.watch('./src/*.html', function(){
@@ -14,6 +18,6 @@ gulp.task('html:watch', function() {
 
 gulp.task('html', function() {
   return gulp.src('./src/*.html')
-    // .pipe(minifyHtml())
+    .pipe(gulpif(env, minifyHtml()))
     .pipe(gulp.dest('./public'));
 });
