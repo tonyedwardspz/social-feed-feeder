eb9b5e3926c10c1da5d9c79278259793a360f0f3
Tony Edwards <tony@plymouthsoftware.com>
2016-12-02 22:12:13 +0000

    Alters order of gulp tasks
    Possible solution to race condition on the server

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
 'use strict';
 
-let gulp = require('gulp'),
-    runSequence = require('run-sequence');
+let gulp = require('gulp');
+let runSequence = require('run-sequence');
 
 // Get tasks from gulp-tasks directory
 require('require-dir')('gulp-tasks');
 
-let allTasks = ['styles', 'scripts', 'copy', 'html', 'images'];
+let allTasks = ['scripts', 'copy', 'html', 'images'];
 gulp.task('default', function(cb) {
   runSequence(
     'clean',
-    // 'bump',
     allTasks,
+    'styles',
     'service-worker',
     cb);
 });
 
 gulp.task('dev', function() {
-  return runSequence('clean', allTasks,
-  'service-worker',
-  'watch', 'nodemon');
+  return runSequence(
+    'clean',
+    allTasks,
+    'styles',
+    'service-worker',
+    'watch',
+    'nodemon');
 });
 
 gulp.task('tests', function(cb) {
   runSequence(
     'clean',
     allTasks,
+    'styles',
     'test:qunit',
     cb);
 });
