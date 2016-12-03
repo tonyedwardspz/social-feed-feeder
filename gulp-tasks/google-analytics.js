e538c2ca840080f849b8cbd6b7619dc3e29b6f12
Tony Edwards <tony@plymouthsoftware.com>
2016-12-03 14:17:37 +0000

    Adds analytics build task

---
+++ gulp-tasks/google-analytics.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let gulp = require('gulp');
+let ga = require('gulp-ga');
+
+// Inject Google analytics code before the closing body tag
+gulp.task('google-analytics', function(){
+  gulp.src('./public/index.html')
+  .pipe(ga({
+    url: 'social-feed-feeder.herokuapp.com/dashboard_index',
+    uid: 'UA-88377745-1',
+    tag: 'body',
+    demographics: true,
+    linkAttribution: true,
+    minify: true,
+    sendPageView: true
+  }))
+  .pipe(gulp.dest('./public/'));
+});
