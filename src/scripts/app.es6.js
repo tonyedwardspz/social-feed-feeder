b8d6baca19d7b8c220a23b45c490fbb693914d5a
Tony Edwards <tony@plymouthsoftware.com>
2016-12-04 21:04:07 +0000

    Cleans up the service worker registration

--- src/scripts/app.es6.js
+++ src/scripts/app.es6.js
@@ -35,35 +35,30 @@ var app;
   // Setup listener for the post attachment
   setupAttachmentChangeListener();
 
-  // document.getElementById('offline-ready').addEventListener('click', (e) => {
-  //   // if (e.target.id === 'dismiss') {
-  //     e.preventDefault();
-  //     console.log('remove offline notificaion');
-  //     document.getElementById('offline-ready').classList.remove('active');
-  //   // }
-  // });
-  //
-  // // Register service worker to enable offline lovelyness
-  // if ('serviceWorker' in navigator) {
-  //   navigator.serviceWorker.register('/service-worker.js').then((registration) => {
-  //     console.log('[SW] Registered');
-  //   }).catch((error) => {
-  //     console.log('[SW] Failed to registered: ', error);
-  //   });
-  //
-  //   // Listen for claiming of our ServiceWorker
-  //   navigator.serviceWorker.addEventListener('controllerchange', function(event) {
-  //     // Listen for changes in the state of our ServiceWorker
-  //     navigator.serviceWorker.controller.addEventListener('statechange', function() {
-  //       console.log('[SW] Set listener', navigator.serviceWorker.state);
-  //       // If the ServiceWorker becomes "activated", let the user know they can go offline!
-  //       if (this.state === 'activated') {
-  //         // Show the "You may now use offline" notification
-  //         document.getElementById('offline-ready').classList.add('active');
-  //
-  //
-  //       }
-  //     });
-  //   });
-  // }
+  document.getElementById('offline-ready').addEventListener('click', (e) => {
+    // if (e.target.id === 'dismiss') {
+      e.preventDefault();
+      console.log('remove offline notificaion');
+      document.getElementById('offline-ready').classList.remove('active');
+    // }
+  });
+
+  // Register service worker to enable offline lovelyness
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('[SW-APP] Registered');
+    }).catch((error) => {
+      console.log('[SW-APP] Failed to registered: ', error);
+    });
+
+    // Listen for changes in the state and notify user when offline ready
+    navigator.serviceWorker.addEventListener('controllerchange', function(event) {
+      navigator.serviceWorker.controller.addEventListener('statechange', function() {
+        console.log('[SW] Set listener', navigator.serviceWorker.state);
+        if (this.state === 'activated') {
+          document.getElementById('offline-ready').classList.add('active');
+        }
+      });
+    });
+  }
 })();
