1315bd7536d6a6928495fecbaab45cd733f80b10
Tony Edwards <tony@plymouthsoftware.com>
2016-12-06 22:14:10 +0000

    Prevents error if ad blocker is running

--- src/scripts/helpers/google-analytics.es6.js
+++ src/scripts/helpers/google-analytics.es6.js
@@ -1,15 +1,18 @@
 'use strict';
 
 /**
-* Sends a navigation event to Google Analytics.
+* Sends a navigation event to Google Analytics if 'ga' is defined. Without the
+* an error is printed to console when an ad block is running.
 * @param {String} view The current view url being displayed
 * @todo Check that this reports correctly in the analytics dashboard
 */
 let updateGoogleAnalytics = view => {
-  ga('send', {
-    hitType: 'event',
-    eventCategory: 'Navigate',
-    eventAction: view,
-    eventLabel: `Navigate to ${view.split('_')[0]} : ${view.split('_')[1]}`
-  });
+  if (ga){
+    ga('send', {
+      hitType: 'event',
+      eventCategory: 'Navigate',
+      eventAction: view,
+      eventLabel: `Navigate to ${view.split('_')[0]} : ${view.split('_')[1]}`
+    });
+  }
 };
