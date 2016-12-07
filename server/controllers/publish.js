783557747357f15b837a6927b29ee34a143765b1
Tony Edwards <tony@plymouthsoftware.com>
2016-12-07 00:15:57 +0000

    Adds sending of an update to buffer

--- server/controllers/publish.js
+++ server/controllers/publish.js
@@ -2,7 +2,7 @@
 
 let BaseController = require('./base');
 let bufferAPI = require('buffer-node');
-let buffer = bufferAPI('{{}}');
+let User = require('../singletons/user-singleton').getInstance().getMongooseModel();
 
 class PublishController extends BaseController {
   constructor() {
@@ -12,7 +12,27 @@ class PublishController extends BaseController {
   publishPosts(req, res) {
     console.log('Publish Posts Hit');
 
-    console.log(req.body);
+    User.findOne({ 'userID': req.cookies.user_id }, (err, user) => {
+      if (err) {
+        console.log('error fetching user while sending posts: ', err);
+        res.send(JSON.stringify({ a: 'Error fetching user while sending posts' }));
+        return;
+      }
+
+      let buffer = bufferAPI(req.cookies.auth_token);
+
+      buffer.updates.create('This is the first update from social feed reader',
+                            user.accountIDS,
+                            null
+                          );
+
+      buffer.updates.create('This is the secons update from social feed reader',
+                            user.accountIDS,
+                            { top: true}
+                          );
+    });
+
+    console.log(req.cookies.auth_token);
   }
 
 
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -18,9 +18,12 @@ class UserController extends BaseController {
   authSuccess(req, res) {
     console.log('[User] Auth Success');
 
+    console.log(req.user);
+
     res.cookie('user_auth', 'true');
     res.cookie('user_id', req.user.userID);
     res.cookie('user_name', req.user.name);
+    res.cookie('auth_token', req.user.accessToken);
     res.writeHead(302, {'Location': '/'});
     res.end();
   }
