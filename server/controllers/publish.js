a182bcfdea6b49e4760d9c987a5354196e959cf3
Tony Edwards <tony@plymouthsoftware.com>
2016-12-15 01:59:55 +0000

    Updates last post date and saves

--- server/controllers/publish.js
+++ server/controllers/publish.js
@@ -21,25 +21,28 @@ class PublishController extends BaseController {
       }
 
       let buffer = bufferAPI(req.cookies.auth_token);
+      let postUpdatePromises = [];
 
+      // find object, send to buffer and update last post date
       Post.getMongooseModel().find({ 'postID': { $in: req.body.posts }}, (err, posts) => {
         posts.forEach(post => {
           let options = {};
           options.media = {};
           options.media.photo = post.attachment;
-          // options.media.image = post.attachment;
-          // options.media.photo = post.attachment;
-          // options.media.image = post.attachment;
-          // if (post.attachment.length > 10) {
-          //   options = {
-          //     media: [ 'photo' post.attachment ]
-          //   }
-          // }
-          console.log(options);
           buffer.updates.create(post.message,
                                 user.accountIDS,
-                                options
-                              );
+                                options);
+
+          postUpdatePromises.push(Post.updatePostDatePromise(post));
+        });
+
+        // Once all dates updated send response
+        Promise.all(postUpdatePromises).then(() => {
+          console.log(`All ${postUpdatePromises.length} posts updated`);
+          res.send('All posts updated');
+        }, (err) => {
+          console.log('Error updating post after scheduling');
+          res.send('No posts updated');
         });
       });
     });
